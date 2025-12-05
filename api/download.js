86bb77cc3be4f6c4a4d9f65b15634b5a563709cc
juniscottmatagalpa <juniscottmatagalpa@gmail.com>
2025-12-05 14:22:23 -0600

    Update download.js

--- api/download.js
+++ api/download.js
@@ -16,6 +16,7 @@ export default async function handler(req, res) {
 
     res.setHeader("Content-Type", "video/mp4");
     res.setHeader("Content-Disposition", `attachment; filename="video.mp4"`);
+
     res.send(buffer);
 
   } catch (e) {
