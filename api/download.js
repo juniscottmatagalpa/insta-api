f1fd9aefb3afbf17af04324a8f9c7cf6c7d60e16
juniscottmatagalpa <juniscottmatagalpa@gmail.com>
2025-12-05 10:12:21 -0600

    Update download.js

--- api/download.js
+++ api/download.js
@@ -1,4 +1,14 @@
 export default async function handler(req, res) {
+
+  // CORS
+  res.setHeader("Access-Control-Allow-Origin", "*");
+  res.setHeader("Access-Control-Allow-Methods", "GET, OPTIONS");
+  res.setHeader("Access-Control-Allow-Headers", "Content-Type");
+
+  if (req.method === "OPTIONS") {
+    return res.status(200).end();
+  }
+
   const url = req.query.url;
   if (!url) return res.status(400).json({ error: "Missing url" });
 
@@ -8,16 +18,9 @@ export default async function handler(req, res) {
 
     const buffer = Buffer.from(await r.arrayBuffer());
 
-    res.setHeader("Access-Control-Allow-Origin", "*");
     res.setHeader("Content-Type", "video/mp4");
     res.setHeader("Content-Disposition", `attachment; filename="video.mp4"`);
-res.setHeader("Access-Control-Allow-Origin", "*");
-res.setHeader("Access-Control-Allow-Methods", "POST, GET, OPTIONS");
-res.setHeader("Access-Control-Allow-Headers", "Content-Type");
 
-
-
-    
     return res.send(buffer);
 
   } catch (e) {
