9bacb5cd15fcfc2ea0eb1a9a657afb0b3e5ed7a6
juniscottmatagalpa <juniscottmatagalpa@gmail.com>
2025-12-05 09:50:57 -0600

    Update download.js

--- api/download.js
+++ api/download.js
@@ -1,16 +1,17 @@
 export default async function handler(req, res) {
   const url = req.query.url;
-  const ext = req.query.ext || "mp4";
   if (!url) return res.status(400).json({ error: "Missing url" });
 
   try {
     const r = await fetch(url);
     if (!r.ok) throw new Error("Archivo no accesible");
+
     const buffer = Buffer.from(await r.arrayBuffer());
 
     res.setHeader("Access-Control-Allow-Origin", "*");
     res.setHeader("Content-Type", "video/mp4");
-    res.setHeader("Content-Disposition", `attachment; filename="video.${ext}"`);
+    res.setHeader("Content-Disposition", `attachment; filename="video.mp4"`);
+
     return res.send(buffer);
 
   } catch (e) {
